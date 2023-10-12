a5926d8ad2a5c564dde450717e58649ce2914c5c
BenjaminKoshyBiju <bennn455@gmail.com>
2023-10-12 21:49:26 +0530

    worked with basic fs functions in node

--- filesNode.js
+++ filesNode.js
@@ -1,11 +1,38 @@
 const fs=require('fs');
-fs.readFile('./docs/hello.txt',(err,data)=>{
-    if(err){
-        console.log(err)
-    }
-    console.log(data.toString())
-})
+// fs.readFile('./docs/hello.txt',(err,data)=>{
+//     if(err){
+//         console.log(err)
+//     }
+//     console.log(data.toString())
+// })
 
-fs.writeFile('./docs/hello.txt','hello world?',()=>{
-    console.log('file written')
-})
+// fs.writeFile('./docs/hello.txt','hello world?',()=>{
+//     console.log('file written') 
+// })
+// if (!fs.existsSync('./Asset'))
+//     {fs.mkdir('./Asset',()=>{
+//     console.log('Directory created')
+//     })
+//     }
+// else{
+//     {fs.rmdir('./Asset',()=>{
+//         console.log('Directory removed')
+//         })}
+//     }
+
+if (fs.existsSync('./docs/hello.txt')){
+
+    fs.unlink('./docs/hello.txt',(err)=>{
+        if(err)
+        {
+            console.log(err)
+        }
+        console.log('file removed');
+    })
+}
+
+else {
+    fs.writeFile('./docs/hello.txt','hello world?',()=>{
+            console.log('file written')})
+
+}
