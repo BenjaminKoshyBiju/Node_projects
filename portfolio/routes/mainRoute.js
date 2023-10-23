5e774e0335271e3c6ebded90edee1bfc2757f7ec
BenjaminKoshyBiju <bennn455@gmail.com>
2023-10-23 15:20:20 +0530

    home page complete
    only miner tweaks left which will be completed after finishing other pages

--- portfolio/routes/mainRoute.js
+++ portfolio/routes/mainRoute.js
@@ -2,6 +2,8 @@ import express from 'express';
 import path from 'path';
 import {skills} from '../data/skills.js';
 import {keywords} from '../data/skills.js';
+import edu from '../data/education.js';
+import work from '../data/work.js';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const router=express.Router()
@@ -9,10 +11,13 @@ router.get('/',(req,res)=>{
     
     res.render('./home',{
         skills:skills,
-        
-        keywords:keywords
+        keywords:keywords,
+        edu:edu,
+        work:work
     });
     
 
 })
+
+
 export default router;
