6fc053c82b43b670491c8c944ce48a20960778a5
BenjaminKoshyBiju <bennn455@gmail.com>
2023-10-28 14:57:02 +0530

    secured api keys of the project

--- news_app/routes/mainRoute.js
+++ news_app/routes/mainRoute.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import config from '../config.js'
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -8,7 +9,10 @@ const __dirname = path.dirname(__filename);
 const router=express.Router()
 router.get('/',(req,res)=>{
     
-    res.render('./home');
+    res.render('./home',{news_apikey:config.news_api,
+                        weather_key:config.weather_api
+
+    });
 })
 
 export default router;
